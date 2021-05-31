8f4c1216b2c3601e462571275bfd103507807238
JJ Chang <53491467+JJ-Chang@users.noreply.github.com>
2021-05-31 19:47:37 -0400

    Add files via upload

---
+++ LabA/a6a05.asm
@@ -0,0 +1,5 @@
+addi x1, x0, 1024
+addi x1, x1, -512
+addi x2, x0, 256
+addi x2, x2, -128
+sub x5, x1, x2
---
+++ LabA/a6b05.asm
@@ -0,0 +1,6 @@
+addi x1, x0, 888
+srli x1, x1, 3
+addi x2, x0, 123
+slli x2, x2, 2
+sub x3, x1, x2
+slli x5, x3, 1
